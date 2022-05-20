a55a1eb86901c9fd5c29e81d101f80d65d2ac162
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-20 17:18:06 -0500

    finished the generateMarkdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  return `# ${data.projectTitle}
+
+## Table of Contents
+* [Project Creator](#projectUsername)
+* [Project Description](#projectDescription)
+* [Project Installation](#projectInstallation)
+* [Project Usage](#projectUsage)
+* [Project Collaborators](#projectCollaborators)
+* [Project License](#projectLicense)
+
+## Creaded by ${data.projectUsername}
+(https://github.com/${data.projectUsername})
+
+## Project Description
+${data.projectDescription}
+
+## Project Installation
+${data.projectInstallation}
+
+## Project Usage
+${data.projectUsage}
+
+## Project Collaborators
+${data.projectCollaborators}
+
+## License
+Repository possesses the ${data.projectLicense} license.
+
+  `;
 }
 
 module.exports = generateMarkdown;
